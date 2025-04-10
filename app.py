b104a6e59636cbac4099ef7f52557ee176402cc5
haris-hm <haris.mehuljic@drake.edu>
2025-04-10 11:21:14 -0500

    Add templates

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-def main():
-    print("Hello from cs178-project-one!")
+from flask import Flask
+from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
-if __name__ == "__main__":
-    main()
+app = Flask(__name__)
+
+@app.route('/login')
+def account():
+    return render_template('login.html')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
